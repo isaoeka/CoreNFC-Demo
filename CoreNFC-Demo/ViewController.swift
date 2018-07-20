0590e176ee26b723891520ee7c33c0d9e712b980
isaoeka <isaoeka@gmail.com>
2018-07-20 19:22:19 +0900

    :pencil2: Initial commit

---
+++ CoreNFC-Demo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoreNFC-Demo
+//
+//  Created by isaoeka on 2018/07/14.
+//  Copyright © 2018年 isaoeka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
